4bc749872f5e9944c97807dc7b21a2ac030c285f
sawanoboly <sawanoboriyu@higanworks.com>
2014-09-30 18:39:36 +0900

    use sudo for chef-client

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 sudo gem install bundler --no-ri --no-rdoc
 bundle install --path vendor/bundle --binstubs
 ./bin/librarian-chef install
-./bin/chef-client -z -o 'sprout-osx-settings::default'
+sudo ./bin/chef-client -z -o 'sprout-osx-settings::default'
